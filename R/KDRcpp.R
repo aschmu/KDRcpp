6bcbfc277d8e6ab2060853a4e7d3df4ceea8d2a0
aschmu <aschmu@users.noreply.github.com>
2016-07-24 13:30:48 +0200

    initial commit

---
+++ R/KDRcpp.R
@@ -0,0 +1,9 @@
+#' @author A. Murangira <murangira@gmail.com>
+#' @docType package
+#' @useDynLib KDRcpp
+#' @importFrom Rcpp sourceCpp
+#' @aliases KDRcpp KDRcpp-package
+#' @references For details on the standard Kernel Dimension Reduction method,
+#' please refer to Fukumizu et. al (2009): \url{https://arxiv.org/abs/0908.1854}
+#' @seealso \code{\link[dr:dr-package]{dr}} for standard dimension reduction
+NULL
